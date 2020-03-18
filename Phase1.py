dcb7ba5dc60d19400ecf4da42367fed26308d724
Mauricio <mauriciosolorio@nevada.unr.edu>
2020-03-18 16:27:12 -0700

    Good good

--- Phase1.py
+++ Phase1.py
@@ -1,5 +1,4 @@
 from pandas.io.html import read_html
-import pandas as pd
 from sqlalchemy import create_engine
 
 # This first phase will retrive the stock prices from all the companies
@@ -8,7 +7,7 @@ url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 wikitables = read_html(url, attrs={"class": "wikitable"})
 
 print("Extracted {num} wikitables".format(num=len(wikitables)))
-# SYM = wikitables[0]['Symbol'].tolist()
 
+# Get all info into a Database for further analysis
 engine = create_engine('mysql+pymysql://mausolorio:ducinALTUM7!@localhost/s&p500')
 wikitables[0].to_sql('info', con=engine, if_exists='replace')
