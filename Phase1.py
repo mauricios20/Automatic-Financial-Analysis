d482fd0a9e8c47c8af53c03ea6739eee3320a642
Mauricio <mauriciosolorio@nevada.unr.edu>
2020-03-20 16:08:20 -0700

    Python codes arrechos bien perros

--- Phase1.py
+++ Phase1.py
@@ -8,6 +8,13 @@ wikitables = read_html(url, attrs={"class": "wikitable"})
 
 print("Extracted {num} wikitables".format(num=len(wikitables)))
 
+# Rename Colum Names - Eliminate Spaces in Names to make Query easy.
+wikitables[0].rename(columns={'GICS Sector': 'Sector',
+                              'GICS Sub Industry': 'Industry',
+                              'Headquarters Location': 'HQ',
+                              'Date first added': 'added'},
+                     inplace=True)
+
 # Get all info into a Database for further analysis
 engine = create_engine('mysql+pymysql://mausolorio:ducinALTUM7!@localhost/s&p500')
 wikitables[0].to_sql('info', con=engine, if_exists='replace')
